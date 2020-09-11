6dec1812c2ba720375f90f66452582ae9c21a780
Guo Jiusen <2434732570@qq.com>
2020-09-11 17:56:56 +0800

    登录检查（拦截器）

--- springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/config/MyMvcConfig.java
+++ springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/config/MyMvcConfig.java
@@ -1,11 +1,14 @@
 package com.jiusen.springbootwebcrud.config;
 
+import com.jiusen.springbootwebcrud.component.LoginHandlerInterceptor;
 import com.jiusen.springbootwebcrud.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //使用
 @Configuration
@@ -16,10 +19,35 @@ public class MyMvcConfig implements WebMvcConfigurer
     {
         registry.addViewController("/success").setViewName("success");
         registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/main.html").setViewName("dashboard");
         System.out.println("123");
     }
 
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean //将组件注册在容器
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/login.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //super.addInterceptors(registry);
+                //静态资源；  *.css , *.js
+                //SpringBoot已经做好了静态资源映射
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/login.html","/","/user/login","/login");
+            }
+        };
+        return adapter;
+    }
     @Bean
     public LocaleResolver localeResolver()
     {
