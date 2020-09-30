397625ba70d71e8c9959daf2669d971153f29384
Guo Jiusen <2434732570@qq.com>
2020-09-30 17:46:40 +0800

    thymeleaf公共部分抽取

--- springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/controller/EmployeeController.java
+++ springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.jiusen.springbootwebcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -16,7 +17,7 @@ public class EmployeeController {
     EmployeeDao employeeDao;
 
     //查询所有员工，返回列表页面
-    @RequestMapping("/emps")
+    @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
 
