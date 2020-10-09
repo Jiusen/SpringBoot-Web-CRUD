4afe127a808cad86da677d706bb2f3088662afa4
Guo Jiusen <2434732570@qq.com>
2020-10-09 11:29:54 +0800

    员工的添加

--- springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/controller/EmployeeController.java
+++ springboot-web-crud/src/main/java/com/jiusen/springbootwebcrud/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.jiusen.springbootwebcrud.controller;
 
+import com.jiusen.springbootwebcrud.dao.DepartmentDao;
 import com.jiusen.springbootwebcrud.dao.EmployeeDao;
+import com.jiusen.springbootwebcrud.entities.Department;
 import com.jiusen.springbootwebcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -15,6 +18,9 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
     //查询所有员工，返回列表页面
     @GetMapping("/emps")
@@ -26,5 +32,29 @@ public class EmployeeController {
         //thymeleaf默认就会拼接
         //classpath:/template/xxxx.html
         return "emp/list";
+    }
+
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面,查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求了请求参数的名字和Javabean入参的对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+
+        System.out.println("保存的员工信息: " + employee);
+
+        //保存员工
+        employeeDao.save(employee);
+        //redirect: 表示重定向到一个地址  /代表当前项目路径
+        //forward: 表示转发到一个地址
+        return "redirect:emps";
     }
 }
